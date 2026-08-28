fde7bca816b37a871c0df4fdc7b41ccffa811404
coded-with-claws <79459323+coded-with-claws@users.noreply.github.com>
2026-08-28 16:49:48 +0200

    feat(taupitaupe): add versus mode (wip - todo conflicts management)

--- taupitaupe.ino
+++ taupitaupe.ino
@@ -11,12 +11,12 @@
 
   bool game_started;
   bool solo_mode;
-  unsigned long start_time, end_time, game_time, tmp_time;
-  unsigned long scoreP1;
-  unsigned long btn_nbr;
-  unsigned short btn_last;
-  bool btn_pressed;
-  bool wrong_btn_pressed;
+  unsigned long start_time, end_timeP1, end_timeP2, game_timeP1, game_timeP2, tmp_time;
+  unsigned long scoreP1, scoreP2;
+  unsigned long btnP1_nbr, btnP2_nbr;
+  unsigned short btnP1_last, btnP2_last;
+  bool btnP1_pressed, btnP2_pressed;
+  bool wrong_btnP1_pressed, wrong_btnP2_pressed;
   uint8_t i;
   
 void setup_taupitaupe() {
@@ -31,7 +31,7 @@ void loop_taupitaupe() {
   if (solo_mode) {
     taupitaupe_solo();
   } else {
-    game_started = false; // TODO VERSUS MODE
+    taupitaupe_vs();
   }
 
 }
@@ -44,13 +44,13 @@ void choose_taupitaupe_solo_vs() {
   while(!game_started) {
 
     lighton_led(P1_1);
-    //lighton_led(P2_1);
+    lighton_led(P2_1);
     tmp_time = millis();
     while(!btn_states[P1_1] && !btn_states[P2_1] && millis() - tmp_time < START_BLINK_DURATION) {
       delay(20); // wait for next ISR call
     }
     lightoff_led(P1_1);
-    //lightoff_led(P2_1);
+    lightoff_led(P2_1);
     tmp_time = millis();
     while(!btn_states[P1_1] && !btn_states[P2_1] && millis() - tmp_time < START_BLINK_DURATION) {
       delay(20); // wait for next ISR call
@@ -70,54 +70,201 @@ void choose_taupitaupe_solo_vs() {
 void taupitaupe_solo() {
   
   scoreP1 = 0;
-  btn_last = 999;
-  btn_pressed = false;
-  wrong_btn_pressed = false;
+  btnP1_last = 999;
+  btnP1_pressed = false;
+  wrong_btnP1_pressed = false;
   delay(1000);
   start_time = millis();
   while(game_started) {
     
     // choose new button
     do {
-      btn_nbr = random(0, BUTTON_NB);
-    } while (btn_nbr == btn_last);
-    btn_last = btn_nbr;
+      btnP1_nbr = random(0, BUTTON_NB);
+    } while (btnP1_nbr == btnP1_last);
+    btnP1_last = btnP1_nbr;
 
-    // light on the button and wait the press
-    lighton_led(btn_nbr);
-    while(!btn_pressed) {
+    // light on the button and wait for the press
+    lighton_led(btnP1_nbr);
+    while(!btnP1_pressed) {
       for (i = 0; i < BUTTON_NB; i++) {
         if (btn_states[i]) {
-          if (i == btn_nbr) {
-            btn_pressed = true;
+          if (i == btnP1_nbr) {
+            btnP1_pressed = true;
           } else {
-            wrong_btn_pressed = true;
+            wrong_btnP1_pressed = true;
           }
         }
       }
       delay(1); // for ISR to keep working
     }
 
-    if (!wrong_btn_pressed) {
-      scoreP1++;
-    }
-    
-    btn_pressed = false;
-    wrong_btn_pressed = false;
-    
-    lightoff_led(btn_nbr);
-
-    if (scoreP1 >= SCORE_MAX) {
-      end_time = millis();
-      for (i = 0; i < BUTTON_NB; i++) {
-        lighton_led(i);
-        delay(100);
-        lightoff_led(i);
+    if (btnP1_pressed) {
+      if (!wrong_btnP1_pressed) {
+        scoreP1++;
       }
+      lightoff_led(btnP1_nbr);
+      btnP1_pressed = false;
+      wrong_btnP1_pressed = false;
+    }
+    
+    lightoff_led(btnP1_nbr);
+
+    // End of game
+    if (scoreP1 >= SCORE_MAX) {
+      end_timeP1 = millis();
       game_started = false;
-      game_time = (end_time - start_time) / 1000;
+      game_timeP1 = (end_timeP1 - start_time) / 1000;
+      
+      for (uint8_t j = 0; j < 3; j++) {
+        for (i = 0; i < BUTTON_NB; i++) {
+          lighton_led(i);
+          delay(100);
+          lightoff_led(i);
+        }
+      }
     }
 
+    delay(150); // important >= 150ms (time for unpress, otherwise next loop iteration the button already pressed will be counted as wrong button)
+  }
+
+}
+
+// VERSUS mode
+void taupitaupe_vs() {
+
+  bool P1wins = false;
+  bool P2wins = false;
+  bool pop_new_btnP1 = true;
+  bool pop_new_btnP2 = true;
+  
+  scoreP1 = 0;
+  scoreP2 = 0;
+  btnP1_last = 999;
+  btnP2_last = 999;
+  btnP1_pressed = false;
+  btnP2_pressed = false;
+  wrong_btnP1_pressed = false;
+  wrong_btnP2_pressed = false;
+  
+  delay(1000);
+  start_time = millis();
+
+  while(game_started) {
+    
+    // choose new button for P1
+    if (pop_new_btnP1) {
+      do {
+        btnP1_nbr = random(0, 6);
+      } while (btnP1_nbr == btnP1_last);
+      btnP1_last = btnP1_nbr;
+      pop_new_btnP1 = false;
+    }
+
+    // choose new button for P2
+    if (pop_new_btnP2) {
+      do {
+        btnP2_nbr = random(6, BUTTON_NB);
+      } while (btnP2_nbr == btnP2_last);
+      btnP2_last = btnP2_nbr;
+      pop_new_btnP2 = false;
+    }
+
+    // light on the buttons and wait for the presses
+    lighton_led(btnP1_nbr);
+    lighton_led(btnP2_nbr);
+    while(!btnP1_pressed && !btnP2_pressed) {
+      // scan P1 buttons
+      for (i = 0; i < 6; i++) {
+        if (btn_states[i]) {
+          if (i == btnP1_nbr) {
+            btnP1_pressed = true;
+          } else {
+            wrong_btnP1_pressed = true;
+          }
+        }
+      }
+      // scan P2 buttons
+      for (i = 6; i < BUTTON_NB; i++) {
+        if (btn_states[i]) {
+          if (i == btnP2_nbr) {
+            btnP2_pressed = true;
+          } else {
+            wrong_btnP2_pressed = true;
+          }
+        }
+      }
+      delay(1); // for ISR to keep working
+    }
+
+    if (btnP1_pressed) {
+      if (!wrong_btnP1_pressed) {
+        scoreP1++;
+      }
+      lightoff_led(btnP1_nbr);
+      btnP1_pressed = false;
+      wrong_btnP1_pressed = false;
+      pop_new_btnP1 = true;
+    }
+
+    if (btnP2_pressed) {
+      if (!wrong_btnP2_pressed) {
+        scoreP2++;
+      }
+      lightoff_led(btnP2_nbr);
+      btnP2_pressed = false;
+      wrong_btnP2_pressed = false;
+      pop_new_btnP2 = true;
+    }
+
+    // end timers ASAP
+    if (scoreP1 >= SCORE_MAX) {
+      game_started = false;
+      P1wins = true;
+      end_timeP1 = millis();
+      if (scoreP2 >= SCORE_MAX) { // Case of tie
+        P2wins = true;
+        end_timeP2 = end_timeP1;
+      }
+    } else if (scoreP2 >= SCORE_MAX) {
+      game_started = false;
+      P2wins = true;
+      end_timeP2 = millis();
+    }
+
+    // End of game - calculate game times
+    if (P1wins && P2wins) {
+      // Case of tie
+      if (scoreP1 == scoreP2) {
+        game_timeP1 = (end_timeP1 - start_time) / 1000;
+        game_timeP2 = (end_timeP2 - start_time) / 1000;
+      } else if (scoreP1 > scoreP2) {
+      // P1 wins
+        game_timeP1 = (end_timeP1 - start_time) / 1000;
+        game_timeP2 = 999999;
+      } else {
+      // P2 wins
+        game_timeP1 = 999999;
+        game_timeP2 = (end_timeP2 - start_time) / 1000;
+      }
+    }
+    // End of game - display win
+    
+    for (uint8_t j = 0; j < 3; j++) {
+      for (i = 0; i < BUTTON_NB; i++) {
+        // light LEDs one at a time to avoid conflicts
+        if (P1wins && i < 6) {
+          lighton_led(i);
+          delay(100);
+          lightoff_led(i);
+        }
+        if (P2wins && i >= 6) {
+          lighton_led(i);
+          delay(100);
+          lightoff_led(i);
+        }
+      }
+    }
+    
     delay(150); // important >= 150ms (time for unpress, otherwise next loop iteration the button already pressed will be counted as wrong button)
   }
 
